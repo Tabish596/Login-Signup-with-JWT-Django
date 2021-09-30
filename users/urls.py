8357e9d9c68ec0ea5b1152478e20bdca21919cb4
Tabish <tabishakhtarsr@gmail.com>
2021-09-30 22:57:02 +0530

    new commit

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, Userview, Logout, Index
+from .views import RegisterView, LoginView, Userview, Logout
 
 app_name = 'basic_app'
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login'),
-    path('userdetails', Userview.as_view(), name='details'),
-    path('logout', Logout.as_view(), name='logout'),
-    path('index', Index.as_view(), name='index'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('userdetails/', Userview.as_view(), name='details'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,61 +1,61 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+from django.shortcuts import redirect, render
 from .serializers import UserSerializer
-from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 import jwt
 import datetime
+from django.views import View
 # Create your views here.
 
 
-class RegisterView(APIView):
+class RegisterView(View):
 
     def get(self, request):
         return render(request, 'basic_app/register.html')
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = UserSerializer(data=request.POST)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return render(request, 'basic_app/index.html')
 
 
-class LoginView(APIView):
+class LoginView(View):
 
     def get(self, request):
         return render(request, 'basic_app/login.html')
 
     def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
 
-        user = User.objects.filter(email=email).first()
-        if user is None:
-            raise AuthenticationFailed('User not found')
-        if not user.check_password(password):
-            raise AuthenticationFailed('Wrong password inserted')
-        payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
-        }
+        if (request.method == 'POST'):
 
-        token = jwt.encode(payload, 'mykey', algorithm='HS256')
+            email = request.POST['email']
+            password = request.POST['password']
 
-        response = Response()
-        response.set_cookie(key='jwt', value=token, httponly=True)
-        response.data = {
-            'jwt': token
-        }
-        return response
-        """ return HttpResponseRedirect(reverse('basic_app:details')) """
+            user = User.objects.filter(email=email).first()
+            if user is None:
+                raise AuthenticationFailed('User not found')
+            if not user.check_password(password):
+                raise AuthenticationFailed('Wrong password inserted')
+            payload = {
+                'id': user.id,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=1),
+                'iat': datetime.datetime.utcnow()
+            }
+
+            token = jwt.encode(payload, 'mykey', algorithm='HS256')
+
+            response = HttpResponseRedirect("/userdetails/")
+            response.set_cookie(key='jwt', value=token, httponly=True)
+            response.data = {
+                'jwt': token
+            }
+            return response
 
 
-class Userview(APIView):
+class Userview(View):
     def get(self, request):
         token = request.COOKIES.get('jwt')
 
@@ -68,38 +68,32 @@ class Userview(APIView):
 
         user = User.objects.filter(id=payload['id']).first()
         serializer = UserSerializer(user)
-        """ return Response(serializer.data) """
         return render(request, 'basic_app/userdetails.html', {'user': serializer.data})
 
     def post(self, request):
         token = request.COOKIES.get('jwt')
         payload = jwt.decode(token, 'mykey', algorithms=['HS256'])
         user = User.objects.filter(id=payload['id']).first()
-        serializer = UserSerializer(user, data=request.data, partial=True)
+        serializer = UserSerializer(user, data=request.POST, partial=True)
         if serializer.is_valid():
             serializer.save()
         return HttpResponseRedirect(reverse('basic_app:details'))
 
     def delete(self, request):
+        response = HttpResponseRedirect("/login/")
         token = request.COOKIES.get('jwt')
         payload = jwt.decode(token, 'mykey', algorithms=['HS256'])
         user = User.objects.get(id=payload['id'])
         user.delete()
-        return HttpResponseRedirect(reverse('basic_app:index'))
-
-
-class Logout(APIView):
-    def post(self, request):
-        response = Response()
         response.delete_cookie('jwt')
-        response.data = {
-            'message': 'Logout Success'
-        }
         return response
 
 
-class Index(APIView):
+class Logout(View):
     def get(self, request):
-        response = Response()
-        response.delete_cookie('jwt')
-        return render()
+        if request.method == 'GET':
+            response = HttpResponseRedirect("/login/")
+            response.delete_cookie('jwt')
+            print(response)
+            return response
+        return redirect("/login/")
